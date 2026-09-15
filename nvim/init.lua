b03dabec7e515d7220de36bcaf10a01f8942ce79
aoesun <37470931+aoesun@users.noreply.github.com>
2026-09-16 00:41:25 +0900

    feat(nvim): add portable Neovim configuration

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+require("config.options")
+require("config.keymaps")
+require("config.completion")
+require("config.treesitter")
+
+require("nvim-autopairs").setup({})
+
+require("config.languages.nix")
